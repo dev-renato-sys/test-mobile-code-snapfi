e63689f90d9be66b3f25619357d53e1663a06c70
Renato Silva <renato_yumi@hotmail.com>
2023-05-20 13:45:39 -0300

    wip

--- lib/src/components/pokemon_about.dart
+++ lib/src/components/pokemon_about.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:snapfi_app/modules/snapfi_app/domain/entities/pokemon_detail_dto.dart';
 
 class PokemonAbout extends StatelessWidget {
