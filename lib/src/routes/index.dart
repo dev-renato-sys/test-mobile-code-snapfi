b8583b14d5216171f67fc45d9b3b35accc9a045b
Renato Silva <renato_yumi@hotmail.com>
2023-05-21 22:08:21 -0300

    wip

--- lib/src/routes/index.dart
+++ lib/src/routes/index.dart
@@ -1,7 +1,3 @@
 import 'package:flutter/material.dart';
-// import 'package:quali_app/src/pages/nqs/index.dart';
 
-Map<String, Widget Function(BuildContext)> routes = {
-  // When navigating to the "/second" route, build the SecondScreen widget.
-  // '/lancarNQ': (context) => const LancarNQ1(),
-};
+Map<String, Widget Function(BuildContext)> routes = {};
