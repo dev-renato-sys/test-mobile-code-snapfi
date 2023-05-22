dcc667c8340dedd78c9d2e6eefbb6d52754ae470
Renato Silva <renato_yumi@hotmail.com>
2023-05-22 14:29:23 -0300

    wip

--- lib/src/components/pokemon_image.dart
+++ lib/src/components/pokemon_image.dart
@@ -1,5 +1,7 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:snapfi_app/modules/snapfi_app/domain/models/pokemon.dart';
+import 'package:snapfi_app/src/theme/light.dart';
 
 class PokemonImage extends StatelessWidget {
   final double width;
@@ -17,8 +19,15 @@ class PokemonImage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Image.network(
-      '$imgUrl${pokemon.getId()}.png',
+    return CachedNetworkImage(
+      placeholder: (context, url) => Transform.scale(
+        scale: 0.2,
+        child: CircularProgressIndicator(
+          color: primaryColor,
+        ),
+      ),
+      errorWidget: (context, url, error) => const Icon(Icons.error),
+      imageUrl: '$imgUrl${pokemon.getId()}.png',
       width: width,
       height: height,
       fit: BoxFit.fill,
